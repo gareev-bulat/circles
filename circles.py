f98edf651e1b99b2a4c89e403efc762025c00249
Bulat <gareevbulich63@gmail.com>
2021-12-07 16:28:25 +0500

    complete

---
+++ circles.py
@@ -0,0 +1,44 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from random import randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.do_paint = False
+        self.setWindowTitle('Генерация окружностей')
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw(self, qp):
+        radius = randint(5, 80)
+        x, y = randint(radius, 800), randint(radius, 510)
+        if radius < x < 800 - radius and radius < y < 510 - radius:
+            color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+            qp.setBrush(color)
+            qp.drawEllipse(x, y, x + radius, x + radius)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
